059736cbb554a180fcac1ffac90f6268fbea4632
Sanusi Abubakr <sanusiabubakr343@gmail.com>
2022-07-28 14:00:05 +0100

    first commit

--- pizza/settings.py
+++ pizza/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -109,21 +109,18 @@ WSGI_APPLICATION = 'pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES={}
 
+if DEBUG:
+    DATABASES["default"]={
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
 
-if DEBUG :
-    DATABASES['default']:{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
-    
-else :
+else:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
     DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
-        
-    
     
 
 
